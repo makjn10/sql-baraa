b8517a0e46bc6c3b0e805b899a48ebde25b1cffe
makjn10 <10.mayank.jain@gmail.com>
2025-06-11 22:53:42 +0530

    Where conditions

---
+++ queries/4_where_conditions.sql
@@ -0,0 +1,79 @@
+use MyDatabase;
+
+-- Comparison operators
+-- Retrieve all customers form Germany
+select *
+from customers
+where country = 'Germany';
+
+-- Retrieve all customers who are not form Germany
+select *
+from customers
+where country != 'Germany';
+
+-- Retrieve all customers with a score greater than 500
+select *
+from customers
+where score > 500;
+
+-- Retrieve all customers with a score of 500 or more
+select *
+from customers
+where score >= 500;
+
+-- Retrieve all customers with a score less than 500
+select *
+from customers
+where score < 500;
+
+-- Retrieve all customers with a score of 500 or less
+select *
+from customers
+where score <= 500;
+
+-- Retrieve all customers from USA and a score greater than 500
+select *
+from customers
+where 
+	country = 'USA' 
+    and score > 500;
+    
+-- Retrieve all customers who are either from USA or have a score greater than 500
+select *
+from customers
+where 
+	country = 'USA' 
+    or score > 500;
+
+-- Retieve all the customers with a score not less than 500
+select *
+from customers
+where not score < 500;
+
+-- Retrieve all customers whose score fall in range between 100 and 500
+select *
+from customers
+where score between 100 and 500;
+
+select *
+from customers
+where score >= 100 and score <= 500;
+
+-- Retrieve customers from either Germany or USA
+select *
+from customers
+where country IN ('Germany', 'USA');
+-- We can use OR operator also but prefer IN
+
+-- Find all customers whose name start with M
+select *
+from customers
+where first_name like 'M%';
+
+-- Find all the customers whose first name ends with n
+select *
+from customers
+where first_name like '%n';
+
+
+
