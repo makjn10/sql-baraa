00bdf355a26f5d3fdfc8e671a4de319b93f8981f
makjn10 <10.mayank.jain@gmail.com>
2025-06-14 13:49:31 +0530

    add null vs empty string vs blank spaces

---
+++ queries/13_null_vs_blank_string.sql
@@ -0,0 +1,7 @@
+-- Use length function to calculate length and differentiate between null, empty string and blank spaces
+
+-- policy1 : use trim function to remove blank spaces to just allow null and empty string
+
+-- policy2 : only use null and avoid empty string and blank spaces. use trim and nullif
+
+-- policy3 : use default value 'unknown' and avoid null, empty string, blank spaces. use trin, nullif and ifnull/coalesce
